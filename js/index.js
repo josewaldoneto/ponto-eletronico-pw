ecb9a8c6fd2059a6fe67ad03339884a1011cdf09
airtonbjunior <airtonbjunior@gmail.com>
2024-08-27 10:44:33 -0300

    Final aula 27-8

--- js/index.js
+++ js/index.js
@@ -2,11 +2,54 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-diaSemana.textContent = "Domingoooooooooo";
+const btnBaterPonto = document.getElementById("btn-bater-ponto");
+btnBaterPonto.addEventListener("click", register);
+
+diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 
-function getCurrentDate() {
+function getWeekDay() {
     const date = new Date();
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return days[date.getDay()];
 }
 
+function getCurrentHour() {
+    // Considerar os métodos abaixo para incluir zeros em numeros < 10
+    // padStart()
+    // slice()
+    // formatos de hora considerando o locale do usuário
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+
+function getCurrentDate() {
+    // Alterar a solução para considerar padStart ou slice
+    // Considerar formatos diferentes da data, conforme localização
+    // do usuário dd/mm/aaaa, mm/dd/aaaa, aaaa/mm/dd, aaaa.mm.dd
+    // Verificar se no Date() há algum método que possa auxiliar
+    // locale
+    const date = new Date();
+    let month = date.getMonth();
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day
+    }
+    if (month < 10) {
+        month = "0" + (month + 1)
+    }
+    return day + "/" + month + "/" + date.getFullYear();
+}
+
+function printCurrentHour() {
+    horaMinSeg.textContent = getCurrentHour();
+}
+
+function register() {
+    // Abrir <dialog> com, no mínimo, 4 botões
+
+    alert("Bater Ponto!");
+}
+
+setInterval(printCurrentHour, 1000);
